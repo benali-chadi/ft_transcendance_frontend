a61d74b90725c64e2b76f26be8e1ae024f1edc05
benali-chadi <benalioulhajchadi@gmail.com>
2022-06-30 01:41:15 +0000

    protected routes, sign up modifications and some formatting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Home from "./Components/Home";
-import Navigation from "./Components/Navigation";
-import Login from "./Components/Login";
+import Home from "./components/Home";
+import Navigation from "./components/Navigation";
+import Login from "./components/Login";
 import { Route, Routes } from "react-router-dom";
 import { useState } from "react";
-import ProtectedRoute from "./Components/common/ProtectedRoute";
+import ProtectedRoute from "./components/common/ProtectedRoute";
 // import logo42 from "./img/42logo.svg"
 
 function App() {
