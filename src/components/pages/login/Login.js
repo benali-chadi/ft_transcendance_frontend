53ef8d406c445c3c8cb1a8360657accd93433ac5
benali-chadi <benalioulhajchadi@gmail.com>
2022-07-23 02:24:53 +0000

    update user card

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,21 +1,21 @@
 import React from "react";
 import Button from "../../common/Button";
-import SignUp from "./Signup";
+// import UpdateUser from "./UpdateUser";
 
 import background from "../../../img/login-background.jpg";
 import logo from "../../../img/logo.png";
 import { ReactComponent as Logo42 } from "../../../img/42logo.svg";
-import { useState } from "react";
+// import { useState } from "react";
 
 const Login = () => {
-	const [showSignUp, setShowSignUp] = useState(false);
+	// const [showUpdateUser, setShowUpdateUser] = useState(false);
 
 	const backgroundStyle = {
 		backgroundImage: `url('${background}')`,
 	};
 
 	const handleLoginClick = () => {
-		// setShowSignUp(!showSignUp);
+		// setShowUpdateUser(!showUpdateUser);
 		document.location.href =
 			"https://api.intra.42.fr/oauth/authorize?client_id=78c3d2bb240eb8f7edc6df1410e59c41f9faab03cf2af5ba88e04e1da9a489a8&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FTest&response_type=code";
 	};
@@ -25,7 +25,9 @@ const Login = () => {
 			className="h-screen w-full bg-gradient-to-r from-[#D8E3F7] to-[#E4CFBA] overflow-auto
                         md:p-12 md:py-20 flex flex-col justify-center items-center"
 		>
-			{showSignUp && <SignUp handleCancelClick={handleLoginClick} />}
+			{/* {showUpdateUser && (
+				<UpdateUser handleCancelClick={handleLoginClick} />
+			)} */}
 
 			<div
 				className="h-screen w-full min-h-max overflow-y-auto bg-50%w bg-no-repeat bg-top flex flex-col justify-end my-auto
