e20ee245ff481cf676ac9f23ef967f0501ec39bc
benali-chadi <benalioulhajchadi@gmail.com>
2022-07-31 01:01:03 +0100

    Routes modifications

--- src/components/common/ProtectedRoute.tsx
+++ src/components/common/ProtectedRoute.tsx
@@ -1,19 +1,19 @@
 import React, { FC } from "react";
-import { Navigate } from "react-router";
+import { Navigate, Location } from "react-router";
 import { userContext } from "../helpers/context";
 
 interface Props {
-	redirectPath?: string;
 	children: JSX.Element;
 	toCheck: boolean;
+	location: Location;
 }
 
-const ProtectedRoute: FC<Props> = ({
-	redirectPath = "/login",
-	children,
-	toCheck,
-}) => {
-	if (toCheck) return <Navigate to={redirectPath} replace />;
+const ProtectedRoute: FC<Props> = ({ children, toCheck, location }) => {
+	if (location.pathname === "/login" && toCheck)
+		return <Navigate to="/" replace />;
+
+	if (location.pathname !== "/login" && toCheck)
+		return <Navigate to={location.pathname} replace />;
 
 	return children;
 };
